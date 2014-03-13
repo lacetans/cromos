bd7c55b6f2dd91a508d788a678d9ef8816a78a06
oscar-hc <oscarhc92@gmail.com>
2014-03-13 19:49:37 +0100

    lopd i lssi
    lopd i lssi

---
+++ lopd/LOPD-LSSI.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LOPD-LSSI</title>
+		<meta charset="UTF-8" />
+
+	</head>
+
+	<body>
+		<h3></h3>
+		<br />
+		<?php
+		echo"<h1>Resum sobre LOPD</h1>
+			<p>La llei orgànica de protecció de dades (LOPD) es una llei que té per objectiu garantitzar i protegir el tractament de dades personals, les llibertats publiques i drets fonamentals de els persones, especialment el honor, intimitat i privacitat personal i familiar.
+			Regula el tractament de dades i fitxers de caràcter personal, independent del suport en el qual estiguin tractats, els drets els ciutadans i les obligacions que aquells que les creen o les tracten.
+			La llei es va crear el 13 de desembre del 1999.</p>
+			<h1>Àmbit d’aplicació LOPD</h1>
+			<p>El àmbit d’aplicació serà a les dades de caràcter personal registrats en el suport físic que els faci susceptibles de tractament, i a tota la modalitat d’un us posterior d’aquestes dades per els sectors públic o privats.
+			La llei estableix que el reglament no serà aplicable a els tractaments de dades referents a persones jurídiques ni als fitxers que es limitin a incorporar les dades de les persones físiques que donin els seus serveis en aquestes, consta de noms i cognoms, funcions o llocs de treball, direcció postal o electrònica, telèfon o fax.</p>
+			<h1>Agencia Espanyola de Protecció de Dades (AEPD)</h1>
+			<p>L’Agència es crea l’any 1993 com una entitat que serà l’encarregada de vetllar pel compliment de les lleis orgàniques, primer la LORTAD i després la LOPD a tot l’Estat espanyol.
+			La funció general de l’Agència Espanyola de Protecció de Dades és la de vetllar pel compliment de la legislació sobre protecció de dades i controlar la seva aplicació, especialment pel que fa als drets d’informació, accés, rectificació, oposició i cancel·lació de dades.</p>
+			<h1>Nivells de seguretat de la LOPD</h1>
+			<p>-Nivell bàsic: inclou totes les dades de caràcter personal.
+			-Nivell mitjà: A més de les dades del nivell basic, s’hi ha d’afegir les relatives a la Hisenda pública, les infraccions, els serveis financers i econòmics o les dades que permetin avaluar la personalitat de la persona.
+			-Nivell alt: A més de les dades del nivell bàsic i del nivell mitjà, s’hi han d’afegir les dades obtingudes per a finalitats policials sense coneixement de la persona afectada i les relatives a la ideologia, religió, sindicals, salut, raça, violència de gènere...
+			Aquestes dades es consideraran “dades especialment protegides”, ja que no es pot obligar ningú a declarar sobre aquestes informacions (ideologia, religió o creences).</p>
+			<h1>Resum sobre LSSICE</h1>
+			<p>La LSSICE surt degut a les noves obligacions per empreses o professionals que
+			realitzen activitats anomenades “serveis de la societat de la informació”, i aquesta llei
+			regula tots els aspectes relacionats amb les activitats o serveis realitzats per medis
+			electrònics com internet.
+			La llei sorgeix el 11 de juliol del 2002.
+			Les obligacions son les següents:
+			-S’ha de fer constar, registrar el nom de domini o direcció de internet.
+			-Oferir de forma permanent, fàcil o gratuïta informació general sobre la empresa.
+			-Deure de col·laboració amb les autoritats.
+			-S’han de conservar les dades de tràfic relatiu a les comunicacions electròniques.</p>
+			<h1>FAQS LSSI</h1>
+			<p><b>Qui està subjecte a la Llei?</b><br/>
+			Les persones que realitzin activitats econòmiques per Internet o altres mitjans
+			telemàtics (correu electrònic, televisió digital interactiva...), sempre que:
+			La direcció i gestió dels seus negocis estigui centralitzada a Espanya o, posseeixi una
+			sucursal, oficina o qualsevol altre tipus establiment permanent situat en territori
+			espanyol, des del qual es dirigeixi la prestació de serveis de la societat de la
+			informació.
+			Es presumiran establerts a Espanya i, per tant, subjectes a la Llei als prestadors de
+			serveis que es trobin inscrits en el Registre Mercantil o en un altre Registre públic
+			espanyol en el qual anés necessària la inscripció per a l'adquisició de personalitat
+			jurídica.
+			La utilització d'un servidor situat a un altre país no serà motiu suficient per descartar la
+			subjecció a la Llei del prestador de serveis. Si les decisions empresarials sobre el
+			contingut o serveis oferts a través d'aquest servidor es prenen en territori espanyol, el
+			prestador es reputarà establert a Espanya.</p>
+			
+			<p><b>Els serveis que es presten de forma gratuïta estan dins de l'àmbit d'aplicació de la Llei?</b><br/>
+			El criteri per determinar si un servei o pàgina web està inclòs dins de l'àmbit d'aplicació
+			de la Llei és si constitueix o no una activitat econòmica per al seu prestador. Tots els
+			serveis que s'ofereixen a canvi d'un preu o contraprestació estan, per tant, subjectes a
+			la nova Llei.
+			No obstant això, el caràcter gratuït d'un servei no determina per si mateix que no
+			estigui subjecte a la Llei. Existeixen multitud de serveis gratuïts oferts a través
+			d'Internet que representen una activitat econòmica per al seu prestador (publicitat,
+			ingressos de patrocinadors, etc.) i, per tant, estarien inclosos dins del seu àmbit
+			d'aplicació. Exemples d'aquests serveis serien els habituals cercadors, o serveis
+			d'enllaços i directoris de pàgines web, així com pàgines finançades amb publicitat o
+			l'enviament de comunicacions comercials.</p>
+			
+			<p><b>S'aplica la LSSI a les Administracions Públiques?</b><br/>
+			En general, la LSSI no s'aplica a les Administracions Públiques, ja que aquestes no tenen el caràcter de prestador de serveis de la societat de la informació definit en el seu annex. D'aquesta forma, determinades activitats típiques de les Administracions, com la gestió electrònica de la recaptació de tributs o la informació sobre els serveis d'un tercer (com podria ser la mera informació a la pàgina web d'un Ajuntament sobre les cases rurals existents en el terme municipal) es consideren com a activitats públiques o d'interès general diferents a la 'activitat econòmica' a la qual es refereix la LSSI.
+			No obstant això, quan l'activitat d'una Administració sí tingui un caràcter econòmic (per exemple, la venda de llibres turístics per una entitat pública depenent d'un Ajuntament), li serà aplicable la LSSI.</p>
+			
+			
+			<p><b>S'aplica la legislació espanyola si un consumidor resident a Espanya compra un producte o contracta un servei a una tenda on-line estrangera?</b><br/>
+			La normativa espanyola s'aplicarà als contractes que els consumidors celebrin amb prestadors establerts a Espanya. El lloc d'establiment a Espanya d'un prestador de serveis ha d'estar indicat a la seva pàgina web i pot comprovar-se mitjançant consulta al Registre Mercantil o un altre que el prestador estigui inscrit.
+			També s'aplicarà la Llei espanyola a les compres que efectuïn a prestadors de serveis establerts en un altre Estat de la Unió Europea o de l'Espai Econòmic Europeu (països de la Unió Europea més Noruega, Islàndia i Liechtenstein), sempre que la normativa espanyola sigui més beneficiosa per al consumidor que la legislació del país en què resideixi el prestador de serveis.
+			Si la compra o la contractació del servei es realitza a un prestador de serveis establert en un país que no pertanyi a l'Espai Econòmic Europeu, la legislació espanyola només serà aplicable si els consumidors espanyols compren en tendes virtuals que dirigeixin la seva activitat al mercat espanyol o s'hagin posat en contacte amb el consumidor a través de correu electrònic.</p>
+						
+			<p><b>La meva empresa té una pàgina web amb informació sobre la seva activitat, productes i serveis que ven, però aquests no poden contractar-se a través de la pàgina web, m'afecten les obligacions per als prestadors de serveis?</b><br/>
+			Sí. La Llei s'aplica a tota activitat amb transcendència econòmica que es realitzi per mitjans electrònics. En aquest cas, l'empresa només està obligada a facilitar, a través de la seva pàgina web, les dades d'informació general establerts en l'article 10 i a assegurar-se que la publicitat d'altres empreses que, si escau, figuri a la pàgina web pugui distingir-se clarament del contingut propi de la pàgina i estigui identificat l'anunciant.</p>
+			
+			<p><b>Quan s'entén que una pàgina web representa una 'activitat econòmica' per al seu titular?</b><br/>
+			Quan aquest percep ingressos directes (per les activitats de comerç electrònic que dugui a terme a través de la pàgina, etc.) o indirectes (per publicitat, patrocini, etc.) derivats de la seva pàgina web, amb independència que aquests permetin sufragar el cost de manteniment de la pàgina, igualin aquesta quantitat o la superin.</p>
+			
+			<p><b>Disposo d'una pàgina web personal. M'afecta la nova Llei?</b><br/>			
+			La Llei no s'aplicarà a una pàgina web personal quan el seu titular no realitzi cap tipus d'activitat econòmica a través de la mateixa.
+			Si la pàgina web té allotjada publicitat en forma de 'bàners', 'pop-*ups', etc., el seu titular estarà subjecte a la Llei si percep alguna remuneració pels mateixos. Si aquests no generen cap ingrés al seu titular, per exemple, per haver estat imposats a canvi de la prestació d'un servei gratuït d'allotjament, aquest no estarà obligat a complir les obligacions previstes en la Llei. Tot això sense perjudici que a aquesta pàgina li afectin altres normes jurídiques que siguin aplicable per ser públicament accessible, com el Codi Penal o la legislació sobre propietat intel·lectual.</p>
+			
+			<p><b>Disposo d'una pàgina web personal, però per finançar despeses tinc allotjats 'bàners' o altres mitjans de publicitat. En què m'afecta la nova Llei?</b><br/>	
+			La Llei és aplicable a les pàgines web que ofereixin missatges publicitaris pels quals el titular de la pàgina percebi algun ingrés. No obstant això, els únics requisits que estableix la Llei quant al contingut de les pàgines d'Internet consisteixen a incloure una informació bàsica a la pàgina web del prestador. Per a una pàgina web personal, la informació que ha de facilitar-se és la següent:
+			1. Nom
+			2. Domicili (indicant, almenys, la localitat i província de residència)
+			3. Adreça de correu electrònic.
+			4. NIF
+			5. Qualsevol dada que permeti establir una comunicació directa i efectiva, com podria ser, per exemple, un telèfon o un fax.
+			6. Els codis de conducta als quals, si escau, estigui adherit i la manera de consultar-los electrònicament.
+			La publicitat que es mostri a la pàgina web haurà d'ajustar-se a l'establert en la Llei, la qual obliga a identificar a l'anunciant i a presentar-la de manera clarament distingible dels continguts no publicitaris de la pàgina. Així mateix, hauran de respectar-se les restants normes sobre publicitat, recollides en altres lleis.</p>
+						
+					
+			
+			";
+		?>
+	</body>
+</html>

4d2cf61cf90550e8faa73b28b0fa64bcd4a2b267
rogerneb <roger.neb@gmail.com>
2014-03-19 18:12:05 +0000

    Provo de fer el primer push

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -31,8 +31,11 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Validar Cromos', 'url'=>array('/validar/gallery')), 
+				array('label'=>'Validar Preguntes', 'url'=>array('/preguntes/validarpreguntes')), 
+				array('label'=>'Validar Imatges', 'url'=>array('/imatges/validarimatges')), 
+				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
